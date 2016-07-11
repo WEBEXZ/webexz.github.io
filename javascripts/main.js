df607258141f03f6f1979aecb42bce4f645e2b6b
WEBEXZ <audi.icon@gmail.com>
2016-07-11 17:50:05 -0500

    Agregando fecha de visita

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,27 @@
 var Informacion = (function() {
 	var _navegador = function() {
 		var usuario = document.getElementById('usuario');
-		usuario.textContent = "Usuario con: " + navigator.appCodeName + " " + navigator.appName;
+		var a = document.createElement("a");
+		a.textContent = "Navegador Web: " + navigator.appCodeName + " " + navigator.appName;
+		usuario.appendChild(a);
+	};
+
+	var _fecha = function() {
+		var usuario = document.getElementById('usuario');
+		var a = document.createElement("a");
+		var date = new Date();
+		var day = date.getDate();
+		var month = date.getMonth();
+		var year = date.getFullYear();
+		var fecha_visita = "Fecha: "+ day + "/" + month + "/" + year;
+		var br = document.createElement("br");
+		usuario.appendChild(br);
+		a.textContent = fecha_visita;
+		usuario.appendChild(a);
 	};
 
 	return {
-		"navegador": _navegador
+		"navegador": _navegador,
+		"fecha": _fecha
 	};
 })();
